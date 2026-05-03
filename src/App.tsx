8f1d011f8f844e3d0e2170fed43d9b2124056a05
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-05-03 22:18:46 +0330

    add: onclick on filter buttons for change filter. delete: filter state from buttons components and add in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import FilterButton from "./components/FilterButton";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+export type FilterType = "all" | "completed" | "noCompleted";
 
 function App() {
+  const [filter, setFilter] = useState<FilterType>("all");
+
   return (
     <div className="container h-svh flex flex-col gap-5 justify-start items-center pt-24">
       <div className="flex justify-between w-full">
         <TodoForm />
-        <FilterButton />
+        <FilterButton setFilter={setFilter} />
       </div>
-      <TodoList />
+      <TodoList filter={filter} />
     </div>
   );
 }
--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-export type FilterType = "all" | "completed" | "noCompleted";
+import type React from "react";
 
-function FilterButton() {
-  const [filter, setFilter] = useState<FilterType>("all");
+function FilterButton({ setFilter }: { setFilter: React.Dispatch<React.SetStateAction<string>> }) {
   return (
     <div className="flex items-center gap-4 [&_button]:text-white [&_button]:bg-blue-500 [&_button]:rounded-lg [&_button]:px-3 [&_button]:py-1">
-      <button>همه</button>
-      <button>تکمیل شده</button>
-      <button>تکمیل نشده</button>
+      <button onClick={() => setFilter("all")}>همه</button>
+      <button onClick={() => setFilter("completed")}>تکمیل شده</button>
+      <button onClick={() => setFilter("noCompleted")}>تکمیل نشده</button>
     </div>
   );
 }
