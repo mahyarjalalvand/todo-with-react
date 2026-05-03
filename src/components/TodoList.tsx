76fce526ab2f44a234192b8288d0a10ac4071052
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-05-03 22:45:14 +0330

    add: switch case for display filtred todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import type { FilterType } from "../App";
 import useTodo from "../hooks/useTodo";
 import TodoItem from "./TodoItem";
 
 function TodoList({ filter }: { filter: FilterType }) {
   const { allTodo } = useTodo();
+  const filtredTodos = allTodo.filter((item) => {
+    switch (filter) {
+      case "completed":
+        return item.isCompleted === true;
+      case "noCompleted":
+        return item.isCompleted === false;
+      default:
+      case "all":
+        return item;
+    }
+  });
   return (
     <div className="w-full border border-gray-300 rounded-lg p-6">
-      {allTodo.length ? (
+      {filtredTodos.length ? (
         <ul className="flex flex-col gap-5">
-          {allTodo.map((item) => (
+          {filtredTodos.map((item) => (
             <TodoItem key={item.id} item={item} />
           ))}
         </ul>
