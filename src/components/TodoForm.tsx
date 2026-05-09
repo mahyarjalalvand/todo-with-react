497387a2721f14859e2153dcc78b67237b498ec8
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-05-09 20:24:11 +0330

    add: function for handler cancle edit mode. add: button for cancle edit

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -4,18 +4,19 @@ import { v4 } from "uuid";
 import type { TodoType } from "../contexts/TodoContext";
 
 function TodoForm() {
-  const { setAllTodo } = useTodo();
+  const { setAllTodo, editTodo, setEditTodo } = useTodo();
   const [todo, setTodo] = useState<string>("");
+
   const allTodoHandler = () => {
     if (!todo.trim()) return;
     const todoItem: TodoType = { id: v4(), title: todo.trim(), isCompleted: false };
     setAllTodo((prv) => [...prv, todoItem]);
     setTodo("");
   };
-
+  const cancleEditHandler = () => setEditTodo(null);
   return (
     <div>
-      <form className="flex items-stretch gap-4">
+      <form className="flex items-stretch gap-4 [&_button]:cursor-pointer">
         <input
           type="text"
           className="outline-0 text-sm border border-blue-300 rounded-lg py-1.5 ps-3 focus:border-blue-500 transition-all duration-200"
@@ -24,9 +25,14 @@ function TodoForm() {
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
         />
-        <button type="button" onClick={allTodoHandler} className="bg-blue-500 py-1.5 rounded-lg px-4 text-sm text-white cursor-pointer">
-          ثبت
+        <button type="button" onClick={allTodoHandler} className="bg-blue-500 py-1.5 rounded-lg px-4 text-sm text-white">
+          {editTodo ? "ویرایش" : "ثبت"}
         </button>
+        {editTodo && (
+          <button type="button" onClick={cancleEditHandler} className="bg-red-500 text-white py-1.5 px-4 rounded-lg text-sm">
+            انصراف
+          </button>
+        )}
       </form>
     </div>
   );
